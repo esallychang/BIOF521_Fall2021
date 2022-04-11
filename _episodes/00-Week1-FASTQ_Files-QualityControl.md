349266cdc0bb5e9f3a774c2c86c616ccced46566
Chang <changes@hg-02161989-lm5.t-mobile.com>
2022-04-11 16:22:25 -0400

    testing R episodes

---
+++ _episodes/00-Week1-FASTQ_Files-QualityControl.md
@@ -0,0 +1,12 @@
+---
+title: "Hands-On: FASTQ Files and Quality Filtering"
+exercises: 60
+objectives:
+- "Assess short reads FASTQ quality using FASTQE 🧬😎 and FastQC"
+- "Perform quality correction with Cutadapt (short reads)"
+- "Summarise quality metrics MultiQC"
+keypoints:
+- "Data can be found on NCBI using the a variety of identifiers for Bioproject, Biosample and SRA"
+- "Galaxy is an open source tool for conducting bioinformatic analyses"
+- "FASTQ files generally store raw sequencing data"
+---
--- data/limma-voom-volcanoplot.R
+++ data/limma-voom-volcanoplot.R
@@ -1,14 +1,3 @@
-
-# Galaxy settings start ---------------------------------------------------
-
-# setup R error handling to go to stderr
-options(show.error.messages = F, error = function() {cat(geterrmessage(), file = stderr()); q("no", 1, F)})
-
-# we need that to not crash galaxy with an UTF8 error on German LC settings.
-loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
-
-# Galaxy settings end -----------------------------------------------------
-
 # Load packages -----------------------------------------------------------
 
 suppressPackageStartupMessages({
